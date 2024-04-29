c8f707088c66deefe22ccdd7172ffc86d0776c98
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-04-29 15:55:59 +0300

    23

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,10 @@
-// Отримуємо посилання на інпут і спан
+
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
-// Прослуховуємо подію 'input'
 nameInput.addEventListener('input', () => {
-  // Отримуємо значення інпута, видаляючи пробіли по краях
   const trimmedValue = nameInput.value.trim();
 
-  // Якщо інпут порожній або містить лише пробіли, підставляємо "Anonymous"
   if (trimmedValue === '') {
     nameOutput.textContent = "Anonymous";
   } else {
