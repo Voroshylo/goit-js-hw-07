77bf2b1ab5f65a6f52576be5bec874076c755476
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-05-01 12:54:58 +0300

    1

--- js/task-4.js
+++ js/task-4.js
@@ -2,25 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('.login-form');
 
   form.addEventListener('submit', (event) => {
-    event.preventDefault(); // предотвращаем перезагрузку страницы при сабмите
+    event.preventDefault();
 
-    const formElements = form.elements; // получаем элементы формы
-    const email = formElements.email.value.trim(); // получение значения из поля Email
-    const password = formElements.password.value.trim(); // получение значения из поля Password
+    const formElements = form.elements;
+    const email = formElements.email.value.trim();
+    const password = formElements.password.value.trim();
 
     if (email === '' || password === '') {
-      // если есть незаполненные поля, показываем alert
       alert('All form fields must be filled in');
     } else {
-      // создаем объект с данными формы
       const formData = {
         email: email,
         password: password,
       };
 
-      console.log(formData); // выводим объект в консоль
+      console.log(formData);
 
-      form.reset(); // очищаем форму
+      form.reset();
     }
   });
 });
